e56294e8edd295bd05c87cfaa316e9e470a2f64d
Rafael Lopez <rafaelukf@gmail.com>
2022-12-15 20:44:30 -0500

    first commit

---
+++ showArray/main.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '.app',
+    data:{
+        product: 'bota',
+        image: './img/vmSocks-green-onWhite.jpg',
+        desc: 'Esta bota fue usada por el mismisimo Napoleon Bonaparte',
+        link: 'https://v2.vuejs.org/v2/guide/',
+        outStock: true,
+        details: ["80% cotton", "20% poliester", "Gender neutral"],
+        variants: [
+            {
+                variantId: 6543, 
+                variantColor: "green"
+            },
+            {
+                variantId: 9875, 
+                variantColor: "blue"
+            }
+        ]
+    }
+})
