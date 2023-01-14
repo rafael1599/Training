abf5d7793e435f5e3738083dd9976cf466fc1207
rafo1599 <rafaelukf@gmail.com>
2023-01-13 22:01:16 -0500

    Starting js on the challenge

--- CHALLENGE/javascript.js
+++ CHALLENGE/javascript.js
@@ -1,7 +1,22 @@
 
 var button = document.querySelector("button");
+let input = document.querySelector("input");
+
+let a = "ai";
+let e = "enter";
+let i = "imes";
+let o = "ober";
+let u = "ufat";
+
 var textoPorEncriptar = document.querySelector("input"); 
 
-if(textoPorEncriptar =="aaa"){
-alert("Funciona");
+function verificar(){
+    if(textoPorEncriptar =="aaa"){
+    alert("Funciona");
+    }
+    else{
+        alert("No es aaa");
+    }
 }
+let button = document.querySelector("jsbutton");
+button.onclick = verificar;
