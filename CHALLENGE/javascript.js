f884d45fbf08fc58676fd64f174a28db6bb3ee62
Rafael <rafaelukf@gmail.com>
2023-01-16 22:52:52 -0500

    js functioning

--- CHALLENGE/javascript.js
+++ CHALLENGE/javascript.js
@@ -1,20 +1,24 @@
 
-let input = document.querySelector("input");
+// Obtener el botón utilizando su clase
+let boton = document.querySelector('.btn__enc');
 
-const txt = document.getElementsByClassName("input__txtArea");
-const btn = document.querySelector("btn__Enc");
-const out = document.getElementsByClassName("res__output")
+let input = document.querySelector(".input__txtArea");
 
+let out = document.getElementsByClassName(".res__output");
 
+// Agregar un evento click al botón
+boton.addEventListener('click', function () {
+   // Obtener el valor del campo de entrada
+   const valor = input.value;
+   let a = "ai";
+   let e = "enter";
+   let i = "imes";
+   let o = "ober";
+   let u = "ufat";
 
-let a = "ai";
-let e = "enter";
-let i = "imes";
-let o = "ober";
-let u = "ufat";
+   // desaparecer 
+   // Mostrar el valor
+   out.innerHTML = input.value;
 
-function verificar(){
-   out.innerHTML = txt.value;
-}
-
-btn.addEventListener('clik', verificar);
+   console.log(valor);
+});
