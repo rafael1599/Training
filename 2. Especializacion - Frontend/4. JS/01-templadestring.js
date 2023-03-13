e5f2ab1e2d6de435e90e2a8fff065db5ef1ee253
Rafael <rafaelukf@gmail.com>
2023-03-13 18:34:58 -0400

    plantillas de texto y arrays

---
+++ 2. Especializacion - Frontend/4. JS/01-templadestring.js
@@ -0,0 +1,10 @@
+const nombre = "Julio";
+const ape ="Ramos";
+
+let nombreCompleto = `El nombre completo es: ${nombre} ${ape}`
+
+function print(variable){
+    console.log(variable);
+}
+
+print(nombreCompleto);
