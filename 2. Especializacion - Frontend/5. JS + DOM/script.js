89141ffc0a6e8094f1924a0bc221d704f1619875
Rafael <rafaelukf@gmail.com>
2023-03-24 23:57:16 -0400

    testing js

---
+++ 2. Especializacion - Frontend/5. JS + DOM/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('[data-form-btn]');
+btn.addEventListener("click", function(){
+    print("prueba para crear tarea")
+})
+
+
+function print(mensaje){
+    console.log(mensaje);
+}
