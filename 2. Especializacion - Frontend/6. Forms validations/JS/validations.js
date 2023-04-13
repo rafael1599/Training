416c8569fe6aee0f09445df9e1a130eb529dddbf
Rafael <rafaelukf@gmail.com>
2023-04-13 12:28:18 -0400

    practicando validaciones

--- 2. Especializacion - Frontend/6. Forms validations/JS/validations.js
+++ 2. Especializacion - Frontend/6. Forms validations/JS/validations.js
@@ -1,4 +1,87 @@
-print("validaciones correctamente conectado")
+const inputNacimiento = document.querySelector("#birth");
+
+inputNacimiento.addEventListener("blur", (evento) => {
+    validarNacimiento(evento.target);
+});
+
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    let mensaje = '';
+    if(mayorDeEdad(fechaCliente)){
+        mensaje = "Debes tener por lo menos 18 years de edad";
+    };
+
+    input.setCustomValidity(mensaje)
+}
+
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    );
+
+    return diferenciaFechas <= fechaActual;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function print(mensaje){
     console.log(mensaje)
