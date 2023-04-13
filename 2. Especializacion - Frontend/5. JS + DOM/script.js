97c63012a5fd8f1232a2bba5b1bc131b1674e6b2
Rafael <rafaelukf@gmail.com>
2023-04-12 22:34:55 -0400

    ToDo app ordered by components

--- 2. Especializacion - Frontend/5. JS + DOM/script.js
+++ 2. Especializacion - Frontend/5. JS + DOM/script.js
@@ -1,4 +1,6 @@
-( () => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteicon.js";
+
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
@@ -25,33 +27,7 @@ const createTask = (evento) => {
 
 btn.addEventListener("click", createTask);
 
-const checkComplete = ()=>{
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
-    return i;
-}
 
-const completeTask =(event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-
-}
-
-const deleteIcon = () =>{
-    const i = document.createElement('i');
-    i.classList.add("fas", "fa-trash-alt", "trashIcon","icon");
-    i.addEventListener("click", deleteTask);
-    return i;
-}
-
-const deleteTask = (event) => {
-    print(event);
-    const parent = event.target.parentElement;
-    parent.remove();
-}
 
 
 
@@ -78,5 +54,3 @@ const deleteTask = (event) => {
 function print(mensaje) {
     console.log(mensaje);
 }
-
-})();
