c17676fa785c27e218d9febe7348095794e3e606
Rafael <rafaelukf@gmail.com>
2023-04-19 23:06:35 -0400

    adding a constructor for cuentaCorriente

--- 2. Especializacion - Frontend/7. POO (Programacion Orientada a Objetos)/index.js
+++ 2. Especializacion - Frontend/7. POO (Programacion Orientada a Objetos)/index.js
@@ -1,41 +1,77 @@
 class cliente{
 nombre;
 dni;
-numeroCuenta;
-saldoCuenta;
+}
+
+class cuentaCorriente{
+    numero;
+    saldo;
+    agencia;
+
+    constructor(){
+        numero = '';
+        saldo = 0;
+        agencia = '';
+        
+    }
+
+    depositoEnCuenta(valor){
+        this.saldo += valor;
+    }
+    retiroDeCuenta(valor){
+        this.saldo = this.saldo - valor;
+    }
 }
 
 const cliente1 = new cliente();
 cliente1.nombre = "Rafael";
 cliente1.dni = 987655468;
-cliente1.numeroCuenta = 6548442135845;
-cliente1.saldoCuenta = 1000;
+
+const cuentaCorriente1 = new cuentaCorriente();
+cuentaCorriente1.numero = 6548442135845;
+cuentaCorriente1.saldo = 1000;
+cuentaCorriente1.agencia = "1";
 
 const cliente2 = new cliente();
 cliente2.nombre = "Leonardo";
 cliente2.dni = 987655468;
-cliente2.numeroCuenta = 6548432185845;
-cliente2.saldoCuenta = 2000;
+
+const cuentaCorriente2 = new cuentaCorriente();
+cuentaCorriente2.numero = 6548432185845;
+cuentaCorriente2.saldo = 2000;
+cuentaCorriente2.agencia = "2"
 
 const cliente3 = new cliente();
 cliente3.nombre = "Carlos";
 cliente3.dni = 987655468;
-cliente3.numeroCuenta = 6548432135896;
-cliente3.saldoCuenta = 3000;
+
+const cuentaCorriente3 = new cuentaCorriente();
+cuentaCorriente3.numero = 6548432135896;
+cuentaCorriente3.saldo = 3000;
+cuentaCorriente3.agencia = "3"
 
 const cliente4 = new cliente();
 cliente4.nombre = "Daniel";
 cliente4.dni = 987655468;
-cliente4.numeroCuenta = 6548432135339;
-cliente4.saldoCuenta = 4000;
+
+const cuentaCorriente4 = new cuentaCorriente();
+cuentaCorriente4.numero = 6548432135339;
+cuentaCorriente4.saldo = 4000;
+cuentaCorriente4.agencia = "4"
 
 const cliente5 = new cliente();
 cliente5.nombre = "Erick";
 cliente5.dni = 987655468;
-cliente5.numeroCuenta = 6548432135456;
-cliente5.saldoCuenta = 5000;
 
-print(cliente2.saldoCuenta);
+const cuentaCorriente5 = new cuentaCorriente();
+cuentaCorriente5.numero = 6548432135456;
+cuentaCorriente5.saldo = 5000;
+cuentaCorriente5.agencia = "5"
+
+cuentaCorriente1.depositoEnCuenta(2);
+cuentaCorriente1.retiroDeCuenta(4);
+print(cliente1);
+print(cuentaCorriente1.saldo)
 
 function print(mensaje){
     console.log(mensaje);
