6f3089be04ad114267ea998940181c54aebe1ce6
Rafael <rafaelukf@gmail.com>
2023-05-12 17:58:40 -0400

    100%

--- 2. Especializacion - Frontend/8. js-herencia-polimorfismo/SistemaAutenticacion.js
+++ 2. Especializacion - Frontend/8. js-herencia-polimorfismo/SistemaAutenticacion.js
@@ -1,6 +1,7 @@
 export class SistemaAutenticacion{
-    login(empleado, clave){
-        if ("autenticable" in usuario && usuario.autenticable instanceof function)
+    static login(empleado, clave){
+        if ("autenticable" in usuario 
+        && usuario.autenticable instanceof Function)
             return usuario.autenticable(clave);     
         else
             return false;
