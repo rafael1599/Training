bfacdfdca7027aed8dcfe61dce8d2fe6bf258827
Rafael <rafaelukf@gmail.com>
2023-05-17 00:31:14 -0400

    53%

--- 2. Especializacion - Frontend/9. Crud-js-Async/service/client-service.js
+++ 2. Especializacion - Frontend/9. Crud-js-Async/service/client-service.js
@@ -1,6 +1,6 @@
-const listaClientes = () => 
-fetch("GET", "http://localhost:3000/perfil").then((respuesta)=> respuesta.json());
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
-export const clientServices= {
-    listaClientes,
+export const clientServices = {
+  listaClientes,
 };
