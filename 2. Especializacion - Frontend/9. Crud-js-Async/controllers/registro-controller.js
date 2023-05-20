187c26177a413aa9d334efd73865b5f20e158e12
Rafael <rafaelukf@gmail.com>
2023-05-19 23:27:35 -0400

    55%

--- 2. Especializacion - Frontend/9. Crud-js-Async/controllers/registro-controller.js
+++ 2. Especializacion - Frontend/9. Crud-js-Async/controllers/registro-controller.js
@@ -1,1 +1,14 @@
+import { clientServices } from "../service/client-service";
 const formulario = document.querySelector("[data-form]");
+
+formulario.addEventListener("sunmit", (evento) => {
+    evento.preventDefault();
+    const nombre = document.querySelector("[data-nombre]").value;
+    const email = document.querySelector("[data-email]").value;
+    console.log(nombre+" - "+email);
+    clientServices
+    .crearCliente(nombre, email)
+    .then(respuesta => {
+        window.location.href = "/screens/registro_completado.html"
+    }).catch(err => console.log(err))
+})
--- 2. Especializacion - Frontend/9. Crud-js-Async/service/client-service.js
+++ 2. Especializacion - Frontend/9. Crud-js-Async/service/client-service.js
@@ -1,6 +1,20 @@
 const listaClientes = () =>
   fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
+export const crearCliente = (nombre, email) => {
+  fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    Headers: {
+      "Content-Type": "aplication/json"
+    }, 
+    body: JSON.stringify({nombre, email})
+  })
+};
+
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
+
+
+
