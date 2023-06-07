0590804e71631448024c36179e33e23e5c57bc06
Rafael <rafaelukf@gmail.com>
2023-06-06 23:43:43 -0400

    React Router 86% - without errors

--- 2. Especializacion - Frontend/11. React2 - react-router-v2-codigoInicial/src/App.js
+++ 2. Especializacion - Frontend/11. React2 - react-router-v2-codigoInicial/src/App.js
@@ -2,7 +2,7 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
-import Page404 from "./pages/Page404"
+import Page404 from './pages/Page404';
 import Header from './components/header';
 import Post from './pages/post';
 import Categoria from './pages/categoria';
@@ -10,21 +10,18 @@ import Categoria from './pages/categoria';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path='/' element = {<Home/>}/>
-        <Route path='/sobre' element = {<Sobre/> }/>
-        <Route path='/posts:id' element = {<Post/>} />
-        <Route path='/categoria' element = {<Categoria/>}/>
-        <Route path='*' element = {<Page404/>}/>
 
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path="/categoria/:id/*" element={<Categoria />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
-
-    </>
   );
 }
 
