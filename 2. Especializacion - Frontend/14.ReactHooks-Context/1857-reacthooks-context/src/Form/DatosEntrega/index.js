46aad6409c2844b67d63b7b60862269077d301ba
Rafael <rafaelukf@gmail.com>
2023-06-09 22:40:38 -0400

    ultimo curso 11%

---
+++ 2. Especializacion - Frontend/14.ReactHooks-Context/1857-reacthooks-context/src/Form/DatosEntrega/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+const DatosEntrega = () => {
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <TextField
+        label="Dirección"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="text"
+      />
+      <TextField
+        label="Ciudad"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="text"
+      />
+      <TextField
+        label="Estado/Provincia"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="text"
+      />
+      <Button variant="contained" type="submit">
+        Crear cuenta
+      </Button>
+    </Box>
+  );
+};
+
+export default DatosEntrega;
---
+++ 2. Especializacion - Frontend/14.ReactHooks-Context/1857-reacthooks-context/src/Form/DatosUsuario/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+class DatosUsuario extends React.Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: {
+        value: '',
+        valid: true
+      },
+      password: {
+        value: '',
+        valid: true
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          label="Correo electrónico"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="email"
+          error={false}
+          helperText={false && "Ingresa un correo electrónico válido"}
+          value={this.state.email.value}
+          onChange={(input) => this.setState({ email:{ value: input.target.value }})}
+        />
+        <TextField
+          label="Contraseña"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="password"
+          value={this.state.password.value}
+          onChange={(input) => this.setState({ password:{ value: input.target.value }})}
+          
+        />
+        <Button variant="contained" type="submit">
+          Siguiente
+        </Button>
+      </Box>
+    );
+  }
+}
+
+export default DatosUsuario;
